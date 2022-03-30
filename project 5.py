6b17bffe693ce35fe93bc4114f9bb2604c074ca1
HITLADBALVANTRAI <101315912+HITLADBALVANTRAI@users.noreply.github.com>
2022-03-30 20:43:27 +0530

    Add files via upload

---
+++ project 5.py
@@ -0,0 +1,10 @@
+
+
+marks = int(input('enter your marks'))
+if ( marks >= 80) :
+    print('Excellent work')
+elif ( marks < 60) :
+    print('Good Work')
+elif( marks < 60 ):
+    print('You can do better')
+
